196b9bf7860cbfb990643e30147c8e7a69176153
Paolo Carnevali <paoloczi@users.noreply.github.com>
2018-10-22 09:45:31 -0700

    Add Blast to dependencies.

--- scripts/InstallPrerequisites-Ubuntu.sh
+++ scripts/InstallPrerequisites-Ubuntu.sh
@@ -7,6 +7,7 @@ apt install -y cmake
 apt install -y libboost-all-dev
 apt install -y libpng-dev
 apt install -y graphviz
+apt install -y ncbi-blast+
 apt install -y hugepages
 apt install -y python3
 apt install -y python3-pip
