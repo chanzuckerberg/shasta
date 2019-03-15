47cb30d45178d45fc50ba431830c6462e7171bc0
Paolo Carnevali <paoloczi@users.noreply.github.com>
2019-03-15 11:25:27 -0700

    Move the definition of ReadFlags to a separate file.

--- src/Assembler.hpp
+++ src/Assembler.hpp
@@ -16,6 +16,7 @@
 #include "MultitreadedObject.hpp"
 #include "OrientedReadPair.hpp"
 #include "ReadGraph.hpp"
+#include "ReadFlags.hpp"
 #include "ReadId.hpp"
 
 // MarginPhase.
@@ -546,38 +547,6 @@ private:
 
 
     // Read flags.
-    class ReadFlags {
-    public:
-
-        // This is set for reads that are approximate palindromic,
-        // that is, are well aligned with their own reverse complement.
-        uint8_t isPalindromic : 1;
-
-        // Set if the read is marked as chimeric.
-        uint8_t isChimeric : 1;
-
-        // Set if the read belongs to a small component of the read graph
-        // that is not used for assembly.
-        // If isChimeric is set, this is also set.
-        uint8_t isInSmallComponent : 1;
-
-        // Strand used when assembling this read.
-        // If 0, the read is assembled unchanged.
-        // If 1, the read is assembled reverse complemented.
-        // Not valid if isPalindromic, isChimeric or isInSmallComponent is set.
-        uint8_t strand : 1;
-
-        // Unused bits.
-        uint8_t bit4 : 1;
-        uint8_t bit5 : 1;
-        uint8_t bit6 : 1;
-        uint8_t bit7 : 1;
-        ReadFlags()
-        {
-            static_assert(sizeof(ReadFlags) == 1, "Unexpected size of ReadFlags.");
-            *reinterpret_cast<uint8_t*>(this) = 0;
-        }
-    };
     MemoryMapped::Vector<ReadFlags> readFlags;
 public:
     void initializeReadFlags();
---
+++ src/ReadFlags.hpp
@@ -0,0 +1,46 @@
+#ifndef CZI_SHASTA_READ_FLAGS_HPP
+#define CZI_SHASTA_READ_FLAGS_HPP
+
+#include <cstdlib>
+#include "cstdint.hpp"
+
+namespace ChanZuckerberg {
+    namespace shasta {
+        class ReadFlags;
+    }
+}
+
+class ChanZuckerberg::shasta::ReadFlags {
+public:
+
+    // This is set for reads that are approximate palindromic,
+    // that is, are well aligned with their own reverse complement.
+    uint8_t isPalindromic : 1;
+
+    // Set if the read is marked as chimeric.
+    uint8_t isChimeric : 1;
+
+    // Set if the read belongs to a small component of the read graph
+    // that is not used for assembly.
+    // If isChimeric is set, this is also set.
+    uint8_t isInSmallComponent : 1;
+
+    // Strand used when assembling this read.
+    // If 0, the read is assembled unchanged.
+    // If 1, the read is assembled reverse complemented.
+    // Not valid if isPalindromic, isChimeric or isInSmallComponent is set.
+    uint8_t strand : 1;
+
+    // Unused bits.
+    uint8_t bit4 : 1;
+    uint8_t bit5 : 1;
+    uint8_t bit6 : 1;
+    uint8_t bit7 : 1;
+    ReadFlags()
+    {
+        static_assert(sizeof(ReadFlags) == 1, "Unexpected size of ReadFlags.");
+        *reinterpret_cast<uint8_t*>(this) = 0;
+    }
+};
+
+#endif
