6de706f53cb8e111ee17a99cfd59ae8fafc583df
ryan <rlorigro@ucsc.edu>
2019-04-22 10:42:37 -0700

    Added median consensus caller part 2

---
+++ src/MedianConsensusCaller.hpp
@@ -0,0 +1,42 @@
+#ifndef CZI_SHASTA_MEDIAN_CONSENSUS_CALLER_HPP
+#define CZI_SHASTA_MEDIAN_CONSENSUS_CALLER_HPP
+
+/*******************************************************************************
+
+A SimpleConsensusCaller is a ConsensusCaller
+that uses a very simple algorithm to compute
+the "best" base and repeat count:
+
+- The best base is the base with the most coverage,
+with ties broken in favor of the "earlier" base in
+ACGT order.
+- The best repeat count is the repeat count with
+the most coverage for the best base as defined above.
+Ties are broken in favor or larger repeat counts.
+Breaking ties in this way, rather than the opposite,
+improved sequence identity a bit, but the caller
+is still biased in favor of deletions.
+
+*******************************************************************************/
+
+
+#include "ConsensusCaller.hpp"
+
+namespace ChanZuckerberg {
+    namespace shasta {
+        class MedianConsensusCaller;
+    }
+}
+
+
+class ChanZuckerberg::shasta::MedianConsensusCaller :
+    public ChanZuckerberg::shasta::ConsensusCaller {
+public:
+
+    size_t predict_runlength(const Coverage &coverage, AlignedBase consensus_base) const;
+    virtual Consensus operator()(const Coverage&) const;
+};
+
+void testMedianConsensusCaller();
+
+#endif
