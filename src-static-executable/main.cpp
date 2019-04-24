46b4052ce662155d5c7e616bcab1181d0624a518
Paolo Carnevali <paoloczi@users.noreply.github.com>
2019-04-24 10:28:22 -0700

    Move class AssemblyOptions to its own files.

--- src-static-executable/main.cpp
+++ src-static-executable/main.cpp
@@ -6,6 +6,7 @@
 
 // Shasta.
 #include "Assembler.hpp"
+#include "AssemblyOptions.hpp"
 #include "buildId.hpp"
 #include "filesystem.hpp"
 #include "timestamp.hpp"
@@ -29,198 +30,15 @@ using namespace shasta;
 
 
 
-// In class AssemblyOptions, we choose names that are
-// consistent with options names in shasta.conf,
-// even though we are violating naming conventions used
-// in the rest of the Shasta code.
-class ChanZuckerberg::shasta::AssemblyOptions {
-public:
-
-
-
-    class ReadsOptions {
-    public:
-        // useRunLengthReads; This will be phased out and always be true.
-        int minReadLength;
-        class PalindromicReadOptions {
-        public:
-            int maxSkip;
-            int maxMarkerFrequency;
-            double alignedFractionThreshold;
-            double nearDiagonalFractionThreshold;
-            int deltaThreshold;
-            void write(ostream& s) const
-            {
-                s << "palindromicReads.maxSkip = " << maxSkip << "\n";
-                s << "palindromicReads.maxMarkerFrequency = " << maxMarkerFrequency << "\n";
-                s << "palindromicReads.alignedFractionThreshold = " << alignedFractionThreshold << "\n";
-                s << "palindromicReads.nearDiagonalFractionThreshold = " << nearDiagonalFractionThreshold << "\n";
-                s << "palindromicReads.deltaThreshold = " << deltaThreshold << "\n";
-            }
-        };
-        PalindromicReadOptions palindromicReads;
-
-        void write(ostream& s) const
-        {
-            s << "[Reads]\n";
-            s << "minReadLength = " << minReadLength << "\n";
-            palindromicReads.write(s);
-        }
-    };
-    ReadsOptions Reads;
-
-
-
-    class KmersOptions {
-    public:
-        int k;
-        double probability;
-        void write(ostream& s) const
-        {
-            s << "[Kmers]\n";
-            s << "k = " << k << "\n";
-            s << "probability = " << probability << "\n";
-        }
-    };
-    KmersOptions Kmers;
-
-
-
-    class MinHashOptions {
-    public:
-        int m;
-        double hashFraction;
-        int minHashIterationCount;
-        int maxBucketSize;
-        int minFrequency;
-        void write(ostream& s) const
-        {
-            s << "[MinHash]\n";
-            s << "m = " << m << "\n";
-            s << "hashFraction = " << hashFraction << "\n";
-            s << "minHashIterationCount = " << minHashIterationCount << "\n";
-            s << "maxBucketSize = " << maxBucketSize << "\n";
-            s << "minFrequency = " << minFrequency << "\n";
-        }
-    };
-    MinHashOptions MinHash;
-
-
-
-    class AlignOptions {
-    public:
-        int maxSkip;
-        int maxMarkerFrequency;
-        int minAlignedMarkerCount;
-        int maxTrim;
-        void write(ostream& s) const
-        {
-            s << "[Align]\n";
-            s << "maxSkip = " << maxSkip << "\n";
-            s << "maxMarkerFrequency = " << maxMarkerFrequency << "\n";
-            s << "minAlignedMarkerCount = " << minAlignedMarkerCount << "\n";
-            s << "maxTrim = " << maxTrim << "\n";
-        }
-    };
-    AlignOptions Align;
-
-
-
-    class ReadGraphOptions {
-    public:
-        int maxAlignmentCount;
-        int minComponentSize;
-        int maxChimericReadDistance;
-        void write(ostream& s) const
-        {
-            s << "[ReadGraph]\n";
-            s << "maxAlignmentCount = " << maxAlignmentCount << "\n";
-            s << "minComponentSize = " << minComponentSize << "\n";
-            s << "maxChimericReadDistance = " << maxChimericReadDistance << "\n";
-        }
-    };
-    ReadGraphOptions ReadGraph;
-
-
-
-    class MarkerGraphOptions {
-    public:
-        int minCoverage;
-        int maxCoverage;
-        int lowCoverageThreshold;
-        int highCoverageThreshold;
-        int maxDistance;
-        int edgeMarkerSkipThreshold;
-        int pruneIterationCount;
-        string simplifyMaxLength;
-        void write(ostream& s) const
-        {
-            s << "[MarkerGraph]\n";
-            s << "minCoverage = " << minCoverage << "\n";
-            s << "maxCoverage = " << maxCoverage << "\n";
-            s << "lowCoverageThreshold = " << lowCoverageThreshold << "\n";
-            s << "highCoverageThreshold = " << highCoverageThreshold << "\n";
-            s << "maxDistance = " << maxDistance << "\n";
-            s << "edgeMarkerSkipThreshold = " << edgeMarkerSkipThreshold << "\n";
-            s << "pruneIterationCount = " << pruneIterationCount << "\n";
-            s << "simplifyMaxLength = " << simplifyMaxLength << "\n";
-        }
-    };
-    MarkerGraphOptions MarkerGraph;
-
-
-
-    class AssemblyOptionsInner {
-    public:
-        int markerGraphEdgeLengthThresholdForConsensus;
-        string consensusCaller;
-        string useMarginPhase;      // False or True
-        string storeCoverageData;   // False or True
-        void write(ostream& s) const
-        {
-            s << "[Assembly]\n";
-            s << "markerGraphEdgeLengthThresholdForConsensus = " <<
-                markerGraphEdgeLengthThresholdForConsensus << "\n";
-            s << "consensusCaller = " <<
-                consensusCaller << "\n";
-            s << "useMarginPhase = " <<
-                useMarginPhase << "\n";
-            s << "storeCoverageData = " <<
-                storeCoverageData << "\n";
-        }
-    };
-    AssemblyOptionsInner Assembly;
-
-
-    void write(ostream& s) const
-    {
-        Reads.write(s);
-        s << "\n";
-        Kmers.write(s);
-        s << "\n";
-        MinHash.write(s);
-        s << "\n";
-        Align.write(s);
-        s << "\n";
-        ReadGraph.write(s);
-        s << "\n";
-        MarkerGraph.write(s);
-        s << "\n";
-        Assembly.write(s);
-        s << endl;
-    }
-};
-
-
-
 void ChanZuckerberg::shasta::shastaMain(int argumentCount, const char** arguments)
 {
+    cout << buildId() << endl;
+
     // Some names in the boost program_options library.
     using boost::program_options::command_line_parser;
     using boost::program_options::options_description;
     using boost::program_options::value;
     using boost::program_options::variables_map;
-
 
     const string executableDescription =
         buildId() +
@@ -237,7 +55,7 @@ void ChanZuckerberg::shasta::shastaMain(int argumentCount, const char** argument
         "Complete documentation for the latest version of Shasta is available here:\n"
         "https://chanzuckerberg.github.io/shasta\n";
 
-    // Options that are only allowed only on the command line.
+    // Options that are only allowed on the command line.
     options_description commandLineOnlyOptions(
         "Options allowed only on the command line");
     string configFileName;
@@ -265,172 +83,7 @@ void ChanZuckerberg::shasta::shastaMain(int argumentCount, const char** argument
     options_description options(
         "Options allowed on the command line and in the config file");
     AssemblyOptions assemblyOptions;
-
-    options.add_options()
-
-        ("Reads.minReadLength", 
-        value<int>(&assemblyOptions.Reads.minReadLength)->
-        default_value(10000),
-        "Read length cutoff.")
-    
-        ("Reads.palindromicReads.maxSkip", 
-        value<int>(&assemblyOptions.Reads.palindromicReads.maxSkip)->
-        default_value(100),
-        "Used for palindromic read detection.")
-    
-        ("Reads.palindromicReads.maxMarkerFrequency", 
-        value<int>(&assemblyOptions.Reads.palindromicReads.maxMarkerFrequency)->
-        default_value(10),
-        "Used for palindromic read detection.")
-    
-        ("Reads.palindromicReads.alignedFractionThreshold", 
-        value<double>(&assemblyOptions.Reads.palindromicReads.alignedFractionThreshold)->
-        default_value(0.1, "0.1"),
-        "Used for palindromic read detection.")
-    
-        ("Reads.palindromicReads.nearDiagonalFractionThreshold", 
-        value<double>(&assemblyOptions.Reads.palindromicReads.nearDiagonalFractionThreshold)->
-        default_value(0.1, "0.1"),
-        "Used for palindromic read detection.")
-
-        ("Reads.palindromicReads.deltaThreshold",
-         value<int>(&assemblyOptions.Reads.palindromicReads.deltaThreshold)->
-         default_value(100),
-         "Used for palindromic read detection.")
-
-        ("Kmers.k",
-        value<int>(&assemblyOptions.Kmers.k)->
-        default_value(10),
-        "Length of marker k-mers (in run-length space).")
-
-        ("Kmers.probability",
-        value<double>(&assemblyOptions.Kmers.probability)->
-        default_value(0.1, "0.1"),
-        "Probability that a k-mer is used as a marker.")
-
-        ("MinHash.m",
-        value<int>(&assemblyOptions.MinHash.m)->
-        default_value(4),
-        "The number of consecutive markers that define a MinHash/LowHash feature.")
-
-        ("MinHash.hashFraction",
-        value<double>(&assemblyOptions.MinHash.hashFraction)->
-        default_value(0.01, "0.01"),
-        "Defines how low a hash has to be to be used with the LowHash algorithm.")
-
-        ("MinHash.minHashIterationCount",
-        value<int>(&assemblyOptions.MinHash.minHashIterationCount)->
-        default_value(10),
-        "The number of MinHash/LowHash iterations.")
-
-        ("MinHash.maxBucketSize",
-        value<int>(&assemblyOptions.MinHash.maxBucketSize)->
-        default_value(10),
-        "The maximum bucket size to be used by the MinHash/LowHash algoritm.")
-
-        ("MinHash.minFrequency",
-        value<int>(&assemblyOptions.MinHash.minFrequency)->
-        default_value(2),
-        "The minimum number of times a pair of reads must be found by the MinHash/LowHash algorithm "
-        "in order to be considered a candidate alignment.")
-
-        ("Align.maxSkip",
-        value<int>(&assemblyOptions.Align.maxSkip)->
-        default_value(30),
-        "The maximum number of markers that an alignment is allowed to skip.")
-
-        ("Align.maxMarkerFrequency",
-        value<int>(&assemblyOptions.Align.maxMarkerFrequency)->
-        default_value(10),
-        "Marker frequency threshold.")
-
-        ("Align.minAlignedMarkerCount",
-        value<int>(&assemblyOptions.Align.minAlignedMarkerCount)->
-        default_value(100),
-        "The minimum number of aligned markers for an alignment to be used.")
-
-        ("Align.maxTrim",
-        value<int>(&assemblyOptions.Align.maxTrim)->
-        default_value(30),
-        "The maximum number of trim markers tolerated at the beginning and end of an alignment.")
-
-        ("ReadGraph.maxAlignmentCount",
-        value<int>(&assemblyOptions.ReadGraph.maxAlignmentCount)->
-        default_value(6),
-        "The maximum alignments to be kept for each read.")
-
-        ("ReadGraph.minComponentSize",
-        value<int>(&assemblyOptions.ReadGraph.minComponentSize)->
-        default_value(100),
-        "The minimum size (number of oriented reads) of a connected component "
-        "of the read graph to be kept.")
-
-        ("ReadGraph.maxChimericReadDistance",
-        value<int>(&assemblyOptions.ReadGraph.maxChimericReadDistance)->
-        default_value(2),
-        "Used for chimeric read detection.")
-
-        ("MarkerGraph.minCoverage",
-        value<int>(&assemblyOptions.MarkerGraph.minCoverage)->
-        default_value(10),
-        "Minimum number of markers for a marker graph vertex.")
-
-        ("MarkerGraph.maxCoverage",
-        value<int>(&assemblyOptions.MarkerGraph.maxCoverage)->
-        default_value(100),
-        "Maximum number of markers for a marker graph vertex.")
-
-        ("MarkerGraph.lowCoverageThreshold",
-        value<int>(&assemblyOptions.MarkerGraph.lowCoverageThreshold)->
-        default_value(0),
-        "Used during approximate transitive reduction.")
-
-        ("MarkerGraph.highCoverageThreshold",
-        value<int>(&assemblyOptions.MarkerGraph.highCoverageThreshold)->
-        default_value(256),
-        "Used during approximate transitive reduction.")
-
-        ("MarkerGraph.maxDistance",
-        value<int>(&assemblyOptions.MarkerGraph.maxDistance)->
-        default_value(30),
-        "Used during approximate transitive reduction.")
-
-        ("MarkerGraph.edgeMarkerSkipThreshold",
-        value<int>(&assemblyOptions.MarkerGraph.edgeMarkerSkipThreshold)->
-        default_value(100),
-        "Used during approximate transitive reduction.")
-
-        ("MarkerGraph.pruneIterationCount",
-        value<int>(&assemblyOptions.MarkerGraph.pruneIterationCount)->
-        default_value(6),
-        "Number of prune iterations.")
-
-        ("MarkerGraph.simplifyMaxLength",
-        value<string>(&assemblyOptions.MarkerGraph.simplifyMaxLength)->
-        default_value("10,100,1000"),
-        "Maximum lengths (in markers) used at each iteration of simplifyMarkerGraph.")
-
-        ("Assembly.markerGraphEdgeLengthThresholdForConsensus",
-        value<int>(&assemblyOptions.Assembly.markerGraphEdgeLengthThresholdForConsensus)->
-        default_value(1000),
-        "Controls assembly of long marker graph edges.")
-
-        ("Assembly.consensusCaller",
-        value<string>(&assemblyOptions.Assembly.consensusCaller)->
-        default_value("SimpleConsensusCaller"),
-        "Selects the consensus caller for repeat counts.")
-
-        ("Assembly.useMarginPhase",
-        value<string>(&assemblyOptions.Assembly.useMarginPhase)->
-        default_value("False"),
-        "Used to turn on margin phase.")
-
-        ("Assembly.storeCoverageData",
-        value<string>(&assemblyOptions.Assembly.storeCoverageData)->
-        default_value("False"),
-        "Used to request storing coverage data.")
-
-        ;
+    assemblyOptions.add(options);
     
 
         
@@ -673,7 +326,8 @@ void ChanZuckerberg::shasta::shastaMain(int argumentCount, const char** argument
     assembler.writeFasta("Assembly.fasta");
 
     // Final disclaimer message.
-    cout << "\nThis run was done using the Shasta static executable,\n"
+    cout << "\n" << buildId() << endl;
+    cout << "This run was done using the Shasta static executable,\n"
         "which provides limited functionality at reduced performance.\n"
         "Depending on run and machine characteristics, the performance penalty\n"
         "can be large (up to a slow down of a factor of 3).\n"
