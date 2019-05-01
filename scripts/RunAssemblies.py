ece0a05a9cf047e2e5797f7c24b665bc34e8b5b2
Paolo Carnevali <paoloczi@users.noreply.github.com>
2019-05-01 12:52:57 -0700

    Working version of RunAssemblies.py.

--- scripts/RunAssemblies.py
+++ scripts/RunAssemblies.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+import datetime
 import glob
 import os
+import shutil
 
 """
 
@@ -9,6 +11,9 @@ This script can be used to run multiple assemblies.
 It assumes that the Shasta executable is in the
 same directory that contains this script
 (typically, that will be the shasta-install/bin directory).
+
+This script requires that the user can run sudo commands
+without being prompted for a password.
 
 The current directory must contain a file called
 samples that lists the sample names to be assembled, one per line.
@@ -21,6 +26,7 @@ The input files for a sample named xyz must be named
 xyz*.fasta
 
 """
+
 
 # Locate the Shasta executable. It is assumed to be in the
 # same directory that contains this script.
@@ -58,12 +64,39 @@ for sample in samples:
 # Main loop over samples.
 for sample in samples:
     
+    # Check that we continue to have sudo access.
+    if not os.system('sudo --non-interactive true') == 0:
+        raise Exception('Unable to run sudo.')
+        
     # Get the input file names for this sample.
     inputFileNames = glob.glob(sample + '*.fasta')
+    print(datetime.datetime.now())
     print('Working on %s with %i input files:' % (sample, len(inputFileNames))) 
     for inputFileName in inputFileNames:
-        print(inputFileName)
+        print(inputFileName, flush=True)
         
     # Run this sample.
+    command = ' '.join([
+        shastaExecutable,
+        '--memoryMode', 'filesystem',
+        '--memoryBacking', '2M', 
+        '--output', sample,
+        '--input'] + inputFileNames) + ' 1>' + sample + '.stdout 2>&1'
+    os.system(command)
+    
+    # Copy the log to the output directory.
+    shutil.copy(sample + '.stdout', sample + '/' + sample + '.stdout')
+    
+    # Check that we continue to have sudo access.
+    if not os.system('sudo --non-interactive true') == 0:
+        raise Exception('Unable to run sudo.')
+
+    # Cleanup the memory.
+    command = ' '.join([
+        shastaExecutable,
+        '--command', 'cleanup',
+        '--output', sample])
+    os.system(command)
+    
             
 
