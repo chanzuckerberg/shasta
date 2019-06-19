6dbff0549f4fd9d5ee927f339933728be1678d50
Paolo Carnevali <paoloczi@users.noreply.github.com>
2019-06-19 13:23:31 -0700

    More work on fixing the MacOS build.

--- scripts/TravisCheckBuildMacOS.sh
+++ scripts/TravisCheckBuildMacOS.sh
@@ -1,7 +1,4 @@
 echo "Checking that the shasta executable was created: shasta-install/bin/shasta"
-#[ -e shasta-install/bin/shasta ]
-
-# For now return success. We know that the MacOS build is broken.
-true
+[ -e shasta-install/bin/shasta ]
 
 
