c3be70d949915e171db7ca67d9396d9e9b42d3ea
Paolo Carnevali <paoloczi@users.noreply.github.com>
2019-06-28 14:06:08 -0700

    Fix tabs.

--- src/MarkerGraph.hpp
+++ src/MarkerGraph.hpp
@@ -10,15 +10,15 @@
 namespace ChanZuckerberg {
     namespace shasta {
 
-    	class MarkerGraph;
+        class MarkerGraph;
 
-    	// Type used to globally identify a marker on an oriented read.
-		// This is the global index of the marker in Assembler::markers.
-		// For a human assembly with coverage 40X the total number
-		// of markers is more than 20 billions (counting both strands),
-		// so this needs to be uint64_t. There could, however, be situations
-		// where uint32_t is sufficient.
-		using MarkerId = uint64_t;
+        // Type used to globally identify a marker on an oriented read.
+        // This is the global index of the marker in Assembler::markers.
+        // For a human assembly with coverage 40X the total number
+        // of markers is more than 20 billions (counting both strands),
+        // so this needs to be uint64_t. There could, however, be situations
+        // where uint32_t is sufficient.
+        using MarkerId = uint64_t;
 
     }
 }
@@ -28,12 +28,12 @@ namespace ChanZuckerberg {
 class ChanZuckerberg::shasta::MarkerGraph {
 public:
 
-	using VertexId = MarkerId;
-	using EdgeId = MarkerId;
-	static const VertexId invalidVertexId;
-	static const EdgeId invalidEdgeId;
+    using VertexId = MarkerId;
+    using EdgeId = MarkerId;
+    static const VertexId invalidVertexId;
+    static const EdgeId invalidEdgeId;
 
-	// To save memory, store vertex ids using 5 bytes.
+    // To save memory, store vertex ids using 5 bytes.
     // This allows for up to 2^40 = 1 Ti markers (both strands).
     // A human size run with 40x coverage and 10% markers
     // has around 25 G markers (both strands).
