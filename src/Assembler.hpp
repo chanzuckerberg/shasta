aac80e0344ab04c45f809134eb66332da56f53dd
Paolo Carnevali <paoloczi@users.noreply.github.com>
2019-07-25 09:31:25 -0700

    Sanity check that the build is on x86_64.

--- src/Assembler.hpp
+++ src/Assembler.hpp
@@ -57,6 +57,12 @@ namespace shasta {
 }
 
 
+// Sanity check that we are compiling on x86_64.
+#if !__x86_64__
+#error "Shasta can only be built on an x86_64 machine (64-bit Intel/AMD)"
+#endif
+
+
 
 // Class used to store various pieces of assembler information in shared memory.
 class shasta::AssemblerInfo {
