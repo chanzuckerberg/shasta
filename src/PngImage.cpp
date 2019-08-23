10e584cda37c5ca4a0590bfe4f5d5ef8b65d5623
PaoloCarnevali <paoloczi@users.noreply.github.com>
2019-08-23 11:17:58 -0700

    Add class PngImage.

---
+++ src/PngImage.cpp
@@ -0,0 +1,75 @@
+#include "PngImage.hpp"
+using namespace shasta;
+
+#include "stdexcept.hpp"
+
+PngImage::PngImage(int width, int height) :
+    width(width),
+    height(height),
+    data(3*width*height)
+{
+}
+
+
+
+void PngImage::setPixel(int x, int y, int r, int g, int b)
+{
+    ::png_byte* pointer = data.data() + 3* (width*y + x);
+    *pointer++ = png_byte(r);
+    *pointer++ = png_byte(g);
+    *pointer++ = png_byte(b);
+}
+
+
+
+void PngImage::write(const std::string& fileName) const
+{
+    // Open the png file.
+    ::FILE* fp = std::fopen (fileName.c_str(), "w");
+    if(!fp) {
+        throw runtime_error("Error opening " + fileName);
+    }
+
+
+    
+    // Initialize the PNG data structures.
+    ::png_structp pngPointer = ::png_create_write_struct (PNG_LIBPNG_VER_STRING, 0, 0, 0);
+    if(pngPointer == 0) {
+        throw runtime_error("Error writing " + fileName);
+    }
+    
+    ::png_infop infoPointer = ::png_create_info_struct (pngPointer);
+    if(infoPointer == 0) {
+        throw runtime_error("Error writing " + fileName);
+    }
+
+   ::png_set_IHDR (
+        pngPointer,
+        infoPointer,
+        width,
+        height,
+        8,
+        PNG_COLOR_TYPE_RGB,
+        PNG_INTERLACE_NONE,
+        PNG_COMPRESSION_TYPE_DEFAULT,
+        PNG_FILTER_TYPE_DEFAULT);
+        
+
+        
+    // Create pointers to rows
+    vector<png_byte*> rowPointers(height);
+    for (int y=0; y<height; y++) {
+        rowPointers[y] = const_cast<png_byte*>(&data[0] + 3*width*y);
+    }
+    
+    // Write.
+    ::png_init_io (pngPointer, fp);
+    ::png_set_rows (pngPointer, infoPointer, rowPointers.data());
+    ::png_write_png (pngPointer, infoPointer, PNG_TRANSFORM_IDENTITY, NULL);
+
+    // Clean up.
+    ::png_destroy_write_struct(&pngPointer, &infoPointer);
+    std::fclose (fp);
+}
+
+
---
+++ src/PngImage.hpp
@@ -0,0 +1,25 @@
+#ifndef SHASTA_PNG_IMAGE_HPP
+#define SHASTA_PNG_IMAGE_HPP
+
+#include <png.h>
+#include "string.hpp"
+#include "vector.hpp"
+
+namespace shasta {
+    class PngImage;
+}
+
+// A simple class to write a png image.
+class shasta::PngImage {
+public:
+    PngImage(int width, int height);
+    void setPixel(int x, int y, int r, int g, int b);
+    void write(const string& fileName) const;
+private:
+    int width;
+    int height;
+    vector<::png_byte> data;
+};
+
+#endif
+
