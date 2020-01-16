3cc53eb70c0e15ffd5e12e5e20281b4ab17be637
PaoloCarnevali <paoloczi@users.noreply.github.com>
2020-01-16 08:43:57 -0800

    Complete rename of MemoryAsContainer to span.

--- src/span.hpp
+++ src/span.hpp
@@ -12,36 +12,32 @@
 #include "string.hpp"
 
 namespace shasta {
-    template<class T> class MemoryAsContainer;
+    template<class T> class span;
 
-    // Output a MemoryAsContainer<char> as a string.
-    inline ostream& operator<<(ostream&, const MemoryAsContainer<char>&);
+    // Output a span<char> as a string.
+    inline ostream& operator<<(ostream&, const span<char>&);
 
-    // Convert a MemoryAsContainer<char> to an integer.
+    // Convert a span<char> to an integer.
     // This cannot be done using std::atol because the
-    // MemoryAsContainer<char> is not null terminated.
-    uint64_t atoul(const MemoryAsContainer<char>&);
+    // span<char> is not null terminated.
+    uint64_t atoul(const span<char>&);
 
-    // Convert a MemoryAsContainer<char> to an std::string.
-    string convertToString(const MemoryAsContainer<char>&);
-
-    // MemoryAsContainer is similar to std::span in C++20.
-    // Add this template typedef to ease the transition.
-    template<class T> using span = MemoryAsContainer<T>;
+    // Convert a span<char> to an std::string.
+    string convertToString(const span<char>&);
 }
 
 
 
-template<class T> class shasta::MemoryAsContainer {
+template<class T> class shasta::span {
 public:
 
-    MemoryAsContainer(T* begin, T* end) :
+    span(T* begin, T* end) :
         dataBegin(begin),
         dataEnd(end)
     {
     }
 
-    MemoryAsContainer() : dataBegin(0), dataEnd(0) {}
+    span() : dataBegin(0), dataEnd(0) {}
 
     size_t size() const
     {
@@ -80,7 +76,7 @@ public:
         return *(dataEnd - 1);
     }
 
-    bool operator==(const MemoryAsContainer<T>& that) const
+    bool operator==(const span<T>& that) const
     {
         if(size() == that.size()) {
             return std::equal(begin(), end(), that.begin());
@@ -88,7 +84,7 @@ public:
             return false;
         }
     }
-    bool operator!=(const MemoryAsContainer<T>& that) const
+    bool operator!=(const span<T>& that) const
     {
         return not((*this) == that);
     }
@@ -99,21 +95,21 @@ private:
 
 
 
-// Output a MemoryAsContainer<char> as a string.
+// Output a span<char> as a string.
 inline std::ostream& shasta::operator<<(
     std::ostream& s,
-    const shasta::MemoryAsContainer<char>&  m)
+    const shasta::span<char>&  m)
 {
     copy(m.begin(), m.end(), ostream_iterator<char>(s));
     return s;
 }
 
 
-// Convert a MemoryAsContainer<char> to an integer.
+// Convert a span<char> to an integer.
 // This cannot be done using std::atol because the
-// MemoryAsContainer<char> is not null terminated.
+// span<char> is not null terminated.
 // The string can only contain numeric characters.
-inline uint64_t shasta::atoul(const MemoryAsContainer<char>& s)
+inline uint64_t shasta::atoul(const span<char>& s)
 {
     uint64_t n = 0;
     for(uint64_t i=0; ; i++) {
@@ -134,8 +130,8 @@ inline uint64_t shasta::atoul(const MemoryAsContainer<char>& s)
 
 
 
-// Convert a MemoryAsContainer<char> to an std::string.
-inline std::string shasta::convertToString(const MemoryAsContainer<char>& m)
+// Convert a span<char> to an std::string.
+inline std::string shasta::convertToString(const span<char>& m)
 {
     return string(m.begin(), m.size());
 }
