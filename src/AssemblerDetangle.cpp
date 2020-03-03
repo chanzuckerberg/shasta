3b8612f97403b91a0e9c11ea488bae32f21aca2e
PaoloCarnevali <paoloczi@users.noreply.github.com>
2020-03-03 07:58:24 -0800

    Create the vertices of the detangled assembly graph.

--- src/AssemblerDetangle.cpp
+++ src/AssemblerDetangle.cpp
@@ -65,5 +65,64 @@ void Assembler::detangle()
         double(assemblerInfo->baseCount) /
         double(markers.totalSize()/2);
     graph.detangle(basesPerMarker);
+
+
+
+    // Use the detangled AssemblyPathGraph to create a new AssemblyGraph.
+    AssemblyGraph newAssemblyGraph;
+
+
+
+    // Create the vertices of the new AssemblyGraph.
+    newAssemblyGraph.vertices.createNew(
+        largeDataName("New-AssemblyGraphVertices"),
+        largeDataPageSize);
+    vector< pair<MarkerGraph::VertexId, AssemblyPathGraph::vertex_descriptor> > newVertices;
+    BGL_FORALL_VERTICES(v, graph, AssemblyPathGraph) {
+        const AssemblyGraph::VertexId oldVertexId = graph[v].vertexId;
+        const MarkerGraph::VertexId markerGraphVertexId = assemblyGraph.vertices[oldVertexId];
+        newVertices.push_back(make_pair(markerGraphVertexId, v));
+    }
+    sort(newVertices.begin(), newVertices.end());
+
+    // The new vertices are sorted by marker graph vertex id.
+    // The position in the newVertices vector is the vertex id in the new assembly graph.
+    newAssemblyGraph.vertices.resize(newVertices.size());
+    for(AssemblyGraph::VertexId vertexId=0; vertexId<newVertices.size(); vertexId++) {
+        newAssemblyGraph.vertices[vertexId] = newVertices[vertexId].first;
+    }
+    cout << "The detangled assembly graph has " <<
+        newVertices.size() << " vertices." << endl;
+
+
+
+    // Find the reverse complement of each vertex.
+    newAssemblyGraph.reverseComplementVertex.createNew(
+        largeDataName("New-AssemblyGraphReverseComplementVertex"), largeDataPageSize);
+    newAssemblyGraph.reverseComplementVertex.resize(newAssemblyGraph.vertices.size());
+    for(AssemblyGraph::VertexId vertexId=0; vertexId<newAssemblyGraph.vertices.size(); vertexId++) {
+        const AssemblyPathGraph::vertex_descriptor v = newVertices[vertexId].second;
+        const AssemblyPathGraphVertex& vertex = graph[v];
+
+        // Use the Rc suffix for reverse complement ids.
+        const AssemblyPathGraph::vertex_descriptor vRc = vertex.reverseComplementVertex;
+        const AssemblyPathGraphVertex& vertexRc = graph[vRc];
+        const AssemblyGraph::VertexId oldAssemblyGraphVertexIdRc = vertexRc.vertexId;
+        const MarkerGraph::VertexId markerGraphVertexIdRc = assemblyGraph.vertices[oldAssemblyGraphVertexIdRc];
+
+        // Look it up in our sorted newVertices vector.
+        const auto it = std::lower_bound(newVertices.begin(), newVertices.end(),
+            make_pair(markerGraphVertexIdRc, vRc));
+        SHASTA_ASSERT(it != newVertices.end());
+        SHASTA_ASSERT(*it == make_pair(markerGraphVertexIdRc, vRc));
+
+        newAssemblyGraph.reverseComplementVertex[vertexId] = it - newVertices.begin();
+    }
+
+    // Sanity check on the reverse complement vertices.
+    for(AssemblyGraph::VertexId vertexId=0; vertexId<newAssemblyGraph.vertices.size(); vertexId++) {
+        const AssemblyGraph::VertexId vertexIdRc = newAssemblyGraph.reverseComplementVertex[vertexId];
+        SHASTA_ASSERT(newAssemblyGraph.reverseComplementVertex[vertexIdRc] == vertexId);
+    }
 }
 
