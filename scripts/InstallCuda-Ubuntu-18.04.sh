992424fdfccad909168639b15e9b88c3c3a3dc26
PaoloCarnevali <paoloczi@users.noreply.github.com>
2020-04-17 11:01:34 -0700

    Use -q option on long wget.

--- scripts/InstallCuda-Ubuntu-18.04.sh
+++ scripts/InstallCuda-Ubuntu-18.04.sh
@@ -14,9 +14,9 @@ cd $tmpDirectoryName
 
 apt-get update
 apt install -y wget
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+wget -q http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
 dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
 apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
 apt-get update
