10c1eb2e0f6a4de481e8ef77beea409f86663701
Yatish Turakhia <yatisht@stanford.edu>
2020-04-19 07:17:20 -0700

    Use CUDA 10.1 instead of 10.2 (#119)

--- scripts/InstallCuda-Ubuntu-18.04.sh
+++ scripts/InstallCuda-Ubuntu-18.04.sh
@@ -14,11 +14,10 @@ cd $tmpDirectoryName
 
 apt-get update
 apt install -y wget
-wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget -q http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 apt-get update
 apt-get -y install cuda
 
