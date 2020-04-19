3684133180e024f2bab9e8553a445ed71fa7b0ec
Ubuntu <ubuntu@ip-172-31-20-127.ec2.internal>
2020-04-19 04:51:20 +0000

    install cuda from network

--- scripts/InstallCuda-Ubuntu-18.04.sh
+++ scripts/InstallCuda-Ubuntu-18.04.sh
@@ -14,13 +14,12 @@ cd $tmpDirectoryName
 
 apt-get update
 apt install -y wget
-wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget -q http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
-apt-get update
-apt-get -y install cuda
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update
+sudo apt-get -y install cuda
 
 # Remove the temporary directory.
 cd 
