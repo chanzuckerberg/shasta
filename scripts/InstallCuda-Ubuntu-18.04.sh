328858ca85c206d6743a37a9feb8cfba285702e7
Ubuntu <ubuntu@ip-172-31-19-42.ec2.internal>
2020-04-19 05:26:08 +0000

    install cuda from network (removing sudo from commands).

--- scripts/InstallCuda-Ubuntu-18.04.sh
+++ scripts/InstallCuda-Ubuntu-18.04.sh
@@ -15,11 +15,11 @@ cd $tmpDirectoryName
 apt-get update
 apt install -y wget
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-sudo apt-get update
-sudo apt-get -y install cuda
+mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+apt-get update
+apt-get -y install cuda
 
 # Remove the temporary directory.
 cd 
