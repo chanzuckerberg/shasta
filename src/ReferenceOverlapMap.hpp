cc30e4205dddb542d5b717261dee5319221dadd9
Ryan Lorig-Roach <rlorigro@ucsc.edu>
2021-02-03 11:30:11 -0800

    More high level documentation

--- src/ReferenceOverlapMap.hpp
+++ src/ReferenceOverlapMap.hpp
@@ -1,10 +1,10 @@
 #ifndef SHASTA_REFERENCEOVERLAPMAP_HPP
 #define SHASTA_REFERENCEOVERLAPMAP_HPP
 
-/// The overlap map is based on a boost interval map. The interval map performs an "aggregation" operation whenever
-/// multiple intervals share space on the number line, combining values for those key:value pairs, and splitting the
-/// intervals at all boundaries. The overlap map uses this data structure for each chromosome/contig in the reference
-/// alignment to infer overlap between reads
+/// This class is an intermediate data structure used to infer overlap from alignment blocks described in a PAF.
+/// This provides a means to construct a reference graph, and search for reference support for any pair of reads
+/// in the candidates/alignments/ReadGraph. In particular, this is used in the Alignment Candidates page in the
+/// shasta browser to label graph visualizations
 
 
 #include "ReadId.hpp"
@@ -30,6 +30,10 @@ namespace shasta{
     class ReferenceOverlapMap;
 }
 
+/// The overlap map is based on a boost interval map. The interval map performs an "aggregation" operation whenever
+/// multiple intervals share space on the number line, combining values for those key:value pairs, and splitting the
+/// intervals at all boundaries. The overlap map uses this data structure for each chromosome/contig in the reference
+/// alignment to infer overlap between reads
 class shasta::ReferenceOverlapMap {
 public:
     unordered_map <string, interval_map <uint32_t, set<OrientedReadId>, total_enricher> > intervals;
