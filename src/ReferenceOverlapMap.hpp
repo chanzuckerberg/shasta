5801889635503946b1cb61c5a1710e90c6389321
Ryan Lorig-Roach <rlorigro@ucsc.edu>
2021-02-03 10:34:16 -0800

    Documentation

--- src/ReferenceOverlapMap.hpp
+++ src/ReferenceOverlapMap.hpp
@@ -1,6 +1,12 @@
 #ifndef SHASTA_REFERENCEOVERLAPMAP_HPP
 #define SHASTA_REFERENCEOVERLAPMAP_HPP
 
+/// The overlap map is based on a boost interval map. The interval map performs an "aggregation" operation whenever
+/// multiple intervals share space on the number line, combining values for those key:value pairs, and splitting the
+/// intervals at all boundaries. The overlap map uses this data structure for each chromosome/contig in the reference
+/// alignment to infer overlap between reads
+
+
 #include "ReadId.hpp"
 
 #include <boost/icl/split_interval_map.hpp>
@@ -24,9 +30,6 @@ namespace shasta{
     class ReferenceOverlapMap;
 }
 
-// The overlap map is based on a boost interval map. The interval map performs an "aggregation" operation whenever
-// multiple intervals share space on the number line, combining values for those key:value pairs, and splitting the
-// intervals at all boundaries.
 class shasta::ReferenceOverlapMap {
 public:
     unordered_map <string, interval_map <uint32_t, set<OrientedReadId>, total_enricher> > intervals;
