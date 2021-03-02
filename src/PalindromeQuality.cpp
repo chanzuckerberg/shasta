0e701854255625f441d2c3264fecf5fa9c10bd3c
Ryan Lorig-Roach <rlorigro@ucsc.edu>
2021-03-01 17:01:51 -0800

    Added palindrome quality classifier

---
+++ src/PalindromeQuality.cpp
@@ -0,0 +1,47 @@
+#include "PalindromeQuality.hpp"
+
+
+bool shasta::classify_palindromic_q_scores(span<char> qualities){
+    bool is_palindromic = false;
+
+    stats_accumulator left_stats;
+    stats_accumulator right_stats;
+
+    auto length = qualities.size();
+
+    // Don't bother classifying any reads that would have less than 3 scores per side
+    if (length < 6){
+        return false;
+    }
+
+    size_t midpoint = length/2;
+
+    for (size_t i=0; i<midpoint; i++){
+        auto q = qualities[i];
+        auto p = quality_char_to_error_probability(q);
+
+        left_stats(p);
+    }
+
+    for (size_t i=midpoint; i<length; i++){
+        auto q = qualities[i];
+        auto p = quality_char_to_error_probability(q);
+
+        right_stats(p);
+    }
+
+    float left_mean = mean(left_stats);
+    float left_variance = variance(left_stats);
+
+    float right_mean = mean(right_stats);
+    float right_variance = variance(right_stats);
+
+    // Compare the mean and variance using thresholds derived empirically from some palindormic reads
+    if (right_mean - left_mean > 0.09 and right_mean >= 0.15){
+        if (right_variance > left_variance and right_variance > 0.025){
+            is_palindromic = true;
+        }
+    }
+
+    return is_palindromic;
+}
---
+++ src/PalindromeQuality.hpp
@@ -0,0 +1,10 @@
+#ifndef SHASTA_PALINDROMEQUALITY_HPP
+#define SHASTA_PALINDROMEQUALITY_HPP
+
+namespace shasta {
+
+bool classify_palindromic_q_scores(span<char> qualities);
+
+}
+
+#endif //SHASTA_PALINDROMEQUALITY_HPP
