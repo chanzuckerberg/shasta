c0bddf743058d2ba3f2aa3f16c9e723fd44797a9
Paolo Carnevali <paoloczi@users.noreply.github.com>
2021-07-26 10:35:45 -0700

    Template versions of commonPrefixLength and commonSuffixLength.

--- src/prefixLength.hpp
+++ src/prefixLength.hpp
@@ -6,35 +6,27 @@
 
 namespace shasta {
 
-    template<class T> uint64_t commonPrefixLength(
-        const vector<T>& x,
-        const vector<T>& y)
+    template<class Container> uint64_t commonPrefixLength(
+        const Container& x,
+        const Container& y)
     {
-        typename vector<T>::const_iterator it;
-
         if(x.size() <= y.size()) {
-            tie(it, ignore) = mismatch(x.begin(), x.end(), y.begin());
-            return it - x.begin();
+            return std::mismatch(x.begin(), x.end(), y.begin()).first - x.begin();
         } else {
-            tie(it, ignore) = mismatch(y.begin(), y.end(), x.begin());
-            return it - y.begin();
+            return std::mismatch(y.begin(), y.end(), x.begin()).first - y.begin();
         }
     }
 
 
 
-    template<class T> uint64_t commonSuffixLength(
-        const vector<T>& x,
-        const vector<T>& y)
+    template<class Container> uint64_t commonSuffixLength(
+        const Container& x,
+        const Container& y)
     {
-        typename vector<T>::const_reverse_iterator it;
-
         if(x.size() <= y.size()) {
-            tie(it, ignore) = mismatch(x.rbegin(), x.rend(), y.rbegin());
-            return it - x.rbegin();
+            return std::mismatch(x.rbegin(), x.rend(), y.rbegin()).first - x.rbegin();
         } else {
-            tie(it, ignore) = mismatch(y.rbegin(), y.rend(), x.rbegin());
-            return it - y.rbegin();
+            return std::mismatch(y.rbegin(), y.rend(), x.rbegin()).first - y.rbegin();
         }
     }
 }
