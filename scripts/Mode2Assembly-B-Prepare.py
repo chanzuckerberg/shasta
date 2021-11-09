3664308086a67c5708231117489aa772cdca75fd
Paolo Carnevali <paoloczi@users.noreply.github.com>
2021-11-09 14:27:41 -0800

    Script to bring binary data in memory before calling Mode2Assembly-B.py.

---
+++ scripts/Mode2Assembly-B-Prepare.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+import os
+import psutil
+
+"""
+
+This copies to /dev/null all binary data needed by Mode2Assembly-B.py.
+Invoke from the assembly directory, which contains the Data directory.
+
+It can be used to get these data in cache, if enough memory is available.
+
+If possible, for best performance, use 
+
+sudo blockdev --setra 65536 /dev/xyz
+
+then when done
+
+sudo blockdev --setra 256 /dev/xyz
+
+where xyz is the block device that contains the assembly directory.
+
+
+"""
+
+files = [
+    'Info', 
+    'Markers.toc', 'Markers.data', 
+    'MarkerGraphVertexTable', 
+    'MarkerGraphVertices.toc', 'MarkerGraphVertices.data',
+    'GlobalMarkerGraphEdges',
+    'GlobalMarkerGraphEdgeMarkerIntervals.toc', 'GlobalMarkerGraphEdgeMarkerIntervals.data',
+    'GlobalMarkerGraphEdgesBySource.toc', 'GlobalMarkerGraphEdgesBySource.data',
+    'GlobalMarkerGraphEdgesByTarget.toc', 'GlobalMarkerGraphEdgesByTarget.data',
+    'MarkerGraphEdgesConsensus.toc', 'MarkerGraphEdgesConsensus.data',
+    'MarkerGraphEdgesConsensusOverlappingBaseCount']
+    
+gb = 1024 * 1024 * 1024
+totalSize = 0
+for file in files:
+    totalSize = totalSize + os.path.getsize("Data/%s" % file)
+print('Total size of the required binary data:', int(round(totalSize / gb)), 'GB')   
+
+memory =  psutil.virtual_memory().total
+print('Total physical memory:', int(round(memory / gb)), 'GB')
+
+
+if totalSize > memory:
+    sys.exit('Not enough memory.')
+    
+
+for file in files:
+    os.system('cp Data/%s /dev/null' % file)
+    
+print('Done')
+
+
+
+
+
