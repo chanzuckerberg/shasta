12953656cd9cf05a481c881c2b740148848a412c
Paolo Carnevali <paoloczi@users.noreply.github.com>
2022-02-22 07:13:43 -0800

    Remove installation of the ogdf library as prerequisite, since we are not going to use it after all.

--- scripts/InstallPrerequisites-macOS.sh
+++ scripts/InstallPrerequisites-macOS.sh
@@ -53,52 +53,6 @@ rm -rf $tmpDirectoryName
 
 
 
-# This builds and installs OGDF.
-# Both the static and shared version of the library are built.
-# It must be run under sudo.
-
-# This installs the following:
-# - Include file directories:
-#       /usr/local/include/ogdf
-#       /usr/local/include/coin
-# - Static libraries:
-#       /usr/local/lib/x86_64-linux-gnu/libOGDF.a
-#       /usr/local/lib/x86_64-linux-gnu/libCOIN.a
-# - Shared libraries:
-#       /usr/local/lib/x86_64-linux-gnu/libOGDF.so
-#       /usr/local/lib/x86_64-linux-gnu/libCOIN.so
-# - Documentation:
-#       /usr/local/share/doc/libogdf
-# - Miscellaneous other files 
-#       /usr/local/lib/x86_64-linux-gnu/cmake/OGDF
-
-# Create a temporary directory to make the OGDF build.
-tmpDirectoryName=$(mktemp --directory --tmpdir)
-cd $tmpDirectoryName
-
-# Get the code.
-git clone https://github.com/ogdf/ogdf.git
-
-# Build the shared library.
-mkdir sharedBuild
-cd sharedBuild
-cmake ../ogdf -DBUILD_SHARED_LIBS=ON
-make -j 8 all
-make install
-cd ..
-
-# Build the static library.
-mkdir staticBuild
-cd staticBuild
-cmake ../ogdf
-make -j 8 all
-make install
-
-# Remove the temporary directory.
-rm -rf $tmpDirectoryName
-
-
-
 echo "==================================="
 echo " Relevant software versions"
 echo "==================================="
