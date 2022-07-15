9b55d1eb2901815da1670f24b6bf80d0f79123eb
Paolo Carnevali <paoloczi@users.noreply.github.com>
2022-07-15 07:57:25 -0700

    Use std::ranges::copy instead of std::copy.

--- src/algorithm.hpp
+++ src/algorithm.hpp
@@ -3,8 +3,12 @@
 
 #include <algorithm>
 
+// With C++20, it is best to use the algorithms in the std::ranges namespace.
+// For more information see:
+// https://en.cppreference.com/w/cpp/algorithm/ranges
+
 namespace shasta {
-    using std::copy;
+    using std::ranges::copy;
     using std::fill;
     using std::find;
     using std::max;
